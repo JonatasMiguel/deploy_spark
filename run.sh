b1a51bbd6811495b7ebecc89996050685fab2e0e
marcosfpr <marcos.rezende@aluno.ufop.edu.br>
2021-01-30 13:54:30 -0300

    small change

--- run.sh
+++ run.sh
@@ -13,15 +13,6 @@ docker push 127.0.0.1:5050/spark
 docker-compose build
 
 echo "--------------------------------------------------------------------"
-echo "Adding the image in docker daemon security exception..."
-echo "--------------------------------------------------------------------"
-
-echo '{
-  "insecure-registries" : ["myregistry:5050"]
-}
-' > /etc/docker/daemon.json
-
-echo "--------------------------------------------------------------------"
 echo "Restarting docker service..."
 echo "--------------------------------------------------------------------"
 
@@ -31,20 +22,8 @@ echo "--------------------------------------------------------------------"
 echo "Deploying in swarm cluster..."
 echo "--------------------------------------------------------------------"
 
-docker stack deploy --compose-file=docker-compose.yml services
-
-echo "--------------------------------------------------------------------"
-echo "Pushing the microservice images in local repository..."
-echo "--------------------------------------------------------------------"
-
-sleep 30
-
-spark_repository=127.0.0.1:5050/spark
-
-echo "--------------------------------------------------------------------"
-echo "Pushing spark image..."
-echo "--------------------------------------------------------------------"
-docker push $spark_repository
+docker stack deploy --compose-file=docker-compose.yml microservices
+docker stack services microservices
 
 echo "--------------------------------------------------------------------"
 echo "End."
