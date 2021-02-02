2c7fa3b07197f854154ea82ba4ece64016c9b2be
marcosfpr <marcos.rezende@aluno.ufop.edu.br>
2021-02-02 10:46:03 -0300

    old

--- services/flask/server.py
+++ services/flask/server.py
@@ -1,5 +1,4 @@
 import os
-import random as rand
 from flask import jsonify, request, Flask, send_file
 from pyspark.context import SparkContext
 from pyspark.sql import SparkSession
@@ -22,9 +21,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    def inside(p):
-        x, y = rand.random(), rand.random()
-        return x*x + y*y < 1
         
     ss = (
         SparkSession
@@ -41,9 +37,9 @@ def index():
     )
     sc: SparkContext = ss.sparkContext
     count = sc.parallelize(range(0, 1000)) \
-        .filter(inside).count()
+        .count()
 
-    return f"<html> <h1> HELLO, WORLD </h1> <h2> Pi is roughly {4.0*count / 100} </h2> </html>"
+    return f"<html> <h1> HELLO, WORLD </h1> </html>"
 
 if __name__ == "__main__":
     app.run(host=os.environ[SAMPLE_HOST_IP],
