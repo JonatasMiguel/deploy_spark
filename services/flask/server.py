d4911143055a63c1fcb91558198bf40c2f94c367
marcosfpr <marcos.rezende@aluno.ufop.edu.br>
2021-02-02 10:16:04 -0300

    old

--- services/flask/server.py
+++ services/flask/server.py
@@ -21,14 +21,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return f"<html> <h1> HELLO, WORLD </h1> </html>"
-
-
-@app.route("/helloworld", methods=["GET"])
-def hello_world(pirange):
     def inside(p):
         x, y = os.urandom.random(), os.urandom.random()
         return x*x + y*y < 1
+        
     ss = (
         SparkSession
         .builder
@@ -46,8 +42,7 @@ def hello_world(pirange):
     count = sc.parallelize(range(0, 1000)) \
         .filter(inside).count()
 
-    return f"<html><h1>Pi is roughly {4.0*count / 100}</h1></html>"
-
+    return f"<html> <h1> HELLO, WORLD </h1> <h2> Pi is roughly {4.0*count / 100} </h2> </html>"
 
 if __name__ == "__main__":
     app.run(host=os.environ[SAMPLE_HOST_IP],
