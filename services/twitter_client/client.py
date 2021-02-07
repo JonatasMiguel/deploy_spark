33a0cb0e6c48ff8d76184cd60502064a763aabbe
marcosfpr <marcos.rezende@aluno.ufop.edu.br>
2021-02-07 12:04:16 -0300

    new service

--- services/twitter_client/client.py
+++ services/twitter_client/client.py
@@ -3,6 +3,7 @@ import json
 import sys
 import socket
 import os
+import logging as log
 
 ACCESS_TOKEN = '911753131087851520-uJbfaAOeIboiKdlBZ8A9yQVqOJWshcN'
 ACCESS_SECRET = '8xPzdiw6ECuWnTGMcdA4BOlATS2AkjPUNBQhLW7U7yRm8'
@@ -28,25 +29,25 @@ class StdOutListener(StreamListener):
         s.bind((TCP_IP, TCP_PORT))
         s.listen(1)
 
-        print("Aguardando uma conexão TCP...")
+        log.info("Aguardando uma conexão TCP...")
         self.conn, self.addr = s.accept()
 
-        print("Conectado... Começando a coletar tweets...")
+        log.info("Conectado... Começando a coletar tweets...")
 
     def on_data(self, data):
         try:
             full_tweet = json.loads(data)
             tweet_text = full_tweet['text']
-            print("Tweet Text: " + tweet_text)
-            print("------------------------------------------")
+            log.info("Tweet Text: " + tweet_text)
+            log.info("------------------------------------------")
             b = bytes(tweet_text + '\n', 'utf-8')
             self.conn.send(b)
         except Exception as e:
-            print(f"Error: {e}")
+            log.info(f"Error: {e}")
         return True
 
     def on_error(self, status):
-        print(status)
+        log.info(status)
         return False
 
 
@@ -59,15 +60,15 @@ if __name__ == "__main__":
     try:
         for i in range(1, len(args)):
             if args[i] == '--help':
-                print("Twitter Client Tool")
-                print("------------------------")
-                print("Usage <python client.py [OPTIONS] [ARGUMENTS]>")
-                print("------------------------")
-                print("[OPTIONS]")
-                print("--help : See the client usage tutorial")
-                print(
+                log.info("Twitter Client Tool")
+                log.info("------------------------")
+                log.info("Usage <python client.py [OPTIONS] [ARGUMENTS]>")
+                log.info("------------------------")
+                log.info("[OPTIONS]")
+                log.info("--help : See the client usage tutorial")
+                log.info(
                     "--track : Set the track for tweets extraction. The tracks need to be separeted by comma.")
-                print("--lang : Set the language in twitter extraction api.\n")
+                log.info("--lang : Set the language in twitter extraction api.\n")
                 sys.exit(1)
             if args[i] == '--track':
                 track = args[i+1]
@@ -76,7 +77,7 @@ if __name__ == "__main__":
                 lang = args[i+1]
                 i += 1
     except IndexError as e:
-        print("Wrong usage. Execute with --help to see more.")
+        log.info("Wrong usage. Execute with --help to see more.")
         sys.exit(1)
         
     track = track.split(',') if track else ['bbb', 'bbb21', "Big brother", "reality"]
